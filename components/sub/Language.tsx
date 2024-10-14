d858aefbad44b6d3f23a934e4aaffee50dcfebff
Jun Hao <yan46648@gmail.com>
2024-10-14 21:49:01 +0630

    add : experience field

--- components/sub/Language.tsx
+++ components/sub/Language.tsx
@@ -23,7 +23,9 @@ const Language = () => {
         className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
       >
         <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
-        <h1 className="Welcome-text text-[13px]">I'm a Chinese in Myanmar</h1>
+        <h1 className="Welcome-text text-[13px]">
+          Myanmar, Chinese and English
+        </h1>
       </motion.div>
 
       <motion.div
